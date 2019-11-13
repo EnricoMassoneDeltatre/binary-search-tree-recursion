37005f427c77bd7a9060c5f4d5757fec7e9bef26
Enrico Massone <enrico.massone@deltatre.it>
2019-11-13 07:11:58 +0100

    added assertions to unit test to check for the node height

--- index.test.js
+++ index.test.js
@@ -132,32 +132,45 @@ test("It is possible to add nodes to the tree", () => {
   // ASSERT
   const objs = target.toObject();
 
+  // root
   expect(objs.value).toEqual(3);
+  expect(objs.height).toEqual(5);
 
+  // left side
   expect(objs.left.value).toEqual(1);
+  expect(objs.left.height).toEqual(2);
   expect(objs.left.left).toBeNull();
 
   expect(objs.left.right.value).toEqual(2);
+  expect(objs.left.right.height).toEqual(1);
   expect(objs.left.right.left).toBeNull();
   expect(objs.left.right.right).toBeNull();
 
+  // right side
   expect(objs.right.value).toEqual(7);
+  expect(objs.right.height).toEqual(4);
 
   expect(objs.right.left.value).toEqual(4);
+  expect(objs.right.left.height).toEqual(3);
   expect(objs.right.left.left).toBeNull();
 
   expect(objs.right.left.right.value).toEqual(6);
+  expect(objs.right.left.right.height).toEqual(2);
   expect(objs.right.left.right.left.value).toEqual(5);
+  expect(objs.right.left.right.left.height).toEqual(1);
   expect(objs.right.left.right.left.right).toBeNull();
   expect(objs.right.left.right.left.left).toBeNull();
 
   expect(objs.right.right.value).toEqual(10);
+  expect(objs.right.right.height).toEqual(3);
   expect(objs.right.right.right).toBeNull();
 
   expect(objs.right.right.left.value).toEqual(9);
+  expect(objs.right.right.left.height).toEqual(2);
   expect(objs.right.right.left.right).toBeNull();
 
   expect(objs.right.right.left.left.value).toEqual(8);
+  expect(objs.right.right.left.left.height).toEqual(1);
   expect(objs.right.right.left.left.right).toBeNull();
   expect(objs.right.right.left.left.left).toBeNull();
 });
